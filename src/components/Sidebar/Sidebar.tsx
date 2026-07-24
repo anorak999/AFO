39ce33f11851767ea70e36fc408d623efdcc93ab
himath rajapaksha <himath.hr@gmail.com>
2026-07-24 15:27:47 +0530

    chore: remove devtools, bump to v3.0.3

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -135,7 +135,7 @@ export default function Sidebar() {
                 transition={{ duration: 0.2 }}
                 className="whitespace-nowrap overflow-hidden"
               >
-                AFO v3.0.2
+                AFO v3.0.3
               </motion.span>
             )}
           </AnimatePresence>
